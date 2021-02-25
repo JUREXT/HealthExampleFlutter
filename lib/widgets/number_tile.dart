61ea70ee279f94e833b82f67226a21aced5756c8
JureLokovsek <lokovsek.jure@gmail.com>
2021-02-25 21:19:54 +0100

    Basic layout setup.

---
+++ lib/widgets/number_tile.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+
+class NumberTile extends StatelessWidget {
+  final Color textColor;
+  final String content;
+
+  NumberTile({this.textColor, this.content});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      padding: EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+        border: Border.all(
+            color: kTileDefaultBorderColor,
+            style: BorderStyle.solid,
+            width: 2.0),
+        borderRadius: BorderRadius.circular(30.0),
+        color: kTileDefaultBackgroundColor,
+      ),
+      child: Text(
+        this.content ?? '',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 30.0,
+          fontWeight: FontWeight.w400,
+          color: this.textColor,
+        ),
+      ),
+    );
+  }
+}
